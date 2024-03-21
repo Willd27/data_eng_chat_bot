debb6a52c45d9a4cf2f73e83b0e6361e181a8a5b
gabriel <60512228+gabriiellllll@users.noreply.github.com>
2024-03-21 14:46:43 +0100

    code push

--- site/chat_bot.py
+++ site/chat_bot.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, jsonify
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
-user_responses = []
+user_responses = {}
 
 app = Flask(__name__)
 
@@ -25,6 +28,59 @@ curr_question = 0
 
 
 
+
+data = pd.read_csv('./Cleaned-Data.csv')
+columns = ['Fever', 'Tiredness', 'Dry-Cough', 'Difficulty-in-Breathing', 'Sore-Throat', 'None_Sympton',
+           'Pains', 'Nasal-Congestion', 'Runny-Nose', 'Diarrhea', 'None_Experiencing', 'Age_0-9', 'Age_10-19',
+           'Age_20-24', 'Age_25-59', 'Age_60+', 'Gender_Female', 'Gender_Male', 'Gender_Transgender', 
+           'Contact_Dont-Know', 'Contact_No', 'Contact_Yes', "Country"]
+
+X = data[columns]
+y = data['Severity_Severe']
+
+# Encoder les variables catégorielles (genre, gravité, etc.)
+X = pd.get_dummies(X,dtype=int)
+
+
+# Fractionner les données en ensembles d'entraînement et de test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Construire le modèle d'arbre de décision
+clf = DecisionTreeClassifier(random_state=42, criterion="entropy")
+clf.fit(X_train, y_train)
+
+def transform_features(features):
+    countries = ['China', 'France', 'Germany', 'Iran', 'Italy', 'Republic of Korean', 'Spain', 'UAE', 'Other', 'Other-EUR']
+    countries_features = {'Country_' + country: 1 if features['country'] == country else 0 for country in countries}
+    genders = ['Female', 'Male', 'Transgender']
+    genders_features = {'Gender_' + gender: 1 if features['gender'] == gender else 0 for gender in genders}
+    age = int(features['age'])
+    age_ranges = {
+        'Age_0-9': 1 if age <= 9 else 0,
+        'Age_10-19': 1 if 9 < age <= 19 else 0,
+        'Age_20-24': 1 if 19 < age <= 24 else 0,
+        'Age_25-59': 1 if 24 < age <= 59 else 0,
+        'Age_60+': 1 if age > 59 else 0
+    }
+    contacts = ["No", "Yes", "Dont-Know"]
+    contact_features = {'Contact_' + contact: 1 if features['contact'] == contact else 0 for contact in contacts}
+    features.pop('country')
+    features.pop('gender')
+    features.pop('age')
+    features.pop('contact')
+    
+    features = {key: 1 if value == 'Yes' else 0 for key, value in features.items()}
+
+    features = {**features, **countries_features, **genders_features, **age_ranges, **contact_features}  
+    print(features)  
+    #return pd.DataFrame([features])
+
+
+
+# Example prediction function
+def predict_severity(features):
+    return clf.predict(pd.DataFrame([features]))[0]
 
 @app.route("/")
 def index():
@@ -51,15 +107,19 @@ def get_response():
 def get_question():
     global curr_question
     global user_responses
-    user_response = request.json["response"]  
+    user_response = request.json["response"]
     column = list(questions.keys())[curr_question - 1]  
-    user_responses.append({column: user_response})
+    user_responses[column] = user_response
     if curr_question < len(questions):
         question = list(questions.values())[curr_question]  
         curr_question += 1  
         bot_response = {"question": question}  
     else:
-        bot_response = {"end": "Thank you for answering all questions."}  
+        bot_response = {"end": "Thank you for answering all questions."}
+        features = transform_features(user_responses)
+        severity = predict_severity(features)
+        print({"severity": severity})
+          
     return jsonify(bot_response)
 
 @app.route("/reset_chat_session", methods=["GET"])
@@ -67,7 +127,7 @@ def reset_chat_session():
     global curr_question
     global user_responses
     curr_question = 0  # Reset the current question index
-    user_responses = []  # Reset answers array
+    user_responses = {}  # Reset answers array
     return jsonify({"message": "Chat session reset successfully."})
 
 
--- site/static/script.js
+++ site/static/script.js
@@ -1,4 +1,3 @@
-// This function sends a message to the server and receives a response
 function sendMessage(message) {
     fetch('/get_question', {
         method: 'POST',
@@ -10,34 +9,28 @@ function sendMessage(message) {
     .then(response => response.json())
     .then(data => {
         if (data.end) {
-            // If the response indicates the end of questions, display a completion message
             displayMessage(data.end);
         } else {
-            // Otherwise, display the next question to the user
             displayQuestion(data.question);
         }
     })
     .catch(error => {
         console.error('Error:', error);
-        // Handle any errors here
     });
 }
 
-// This function displays a question to the user
 function displayQuestion(question) {
     const chatBox = document.getElementById('chat-box');
     question = 'Bot: ' + question + '<br>';
     chatBox.innerHTML += `<div class="bot-message">${question}</div>`;
 }
 
-// This function displays a message from the server
 function displayMessage(message) {
     const chatBox = document.getElementById('chat-box');
     message = 'You: ' + message + '<br>';
     chatBox.innerHTML += `<div class="bot-message">${message}</div>`;
 }
 
-// This function is called when the user clicks the "Send" button
 function sendMessageHandler() {
     const userInput = document.getElementById('user-input');
     const message = userInput.value;
@@ -46,11 +39,9 @@ function sendMessageHandler() {
     sendMessage(message); // Send the user's message to the server
 }
 
-// Add event listener to the "Send" button
 document.getElementById('send-button').addEventListener('click', sendMessageHandler);
 
 
-// Function to reset the chat session and recover all answers
 function resetChatSession() {
     const chatBox = document.getElementById('chat-box');
     chatBox.innerHTML = 'Welcome to COVID BOT'; // Clear chat history
@@ -65,7 +56,34 @@ function resetChatSession() {
         console.error('Error:', error);
         // Handle any errors here
     });
+    initiateConversation();
 }
 
 // Add event listener to the "Reset" button
 document.getElementById('reset-button').addEventListener('click', resetChatSession);
+
+
+// Add this function to initiate the conversation when the page loads
+function initiateConversation() {
+    fetch('/get_question', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ response: '' }), // Send an empty message initially
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.question) {
+            displayQuestion(data.question); // Display the first question
+        } else {
+            console.error('Unexpected response:', data);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Call initiateConversation when the page loads
+window.addEventListener('load', resetChatSession);
