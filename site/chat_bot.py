894c2eb161a994db3efee9e9a54e65eba474f46f
gabriel <60512228+gabriiellllll@users.noreply.github.com>
2024-03-21 11:06:59 +0100

    Add runner and questions

--- site/chat_bot.py
+++ site/chat_bot.py
@@ -2,7 +2,23 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-questions = {"name": "What is your name?", "age": "How old are you?"}
+questions : {
+    'country': 'What country are you from ? [China, France, Germany, Iran, Spain, UAE, Republic of Korean, Other, Other-EUR]',
+    'contact': 'Have you been in contact with someone who has covid? [Yes, No, Dont-Know]',
+    'gender': 'What is your gender ? [Female, Male, Transgender]',
+    'age': 'What is your age ?',
+    'None_Experiencing' : 'Have you ever had covid? [Yes, No]',
+    'Diarrhea' : 'Do you have Diarrhea? [Yes, No]',
+    'Runny-Nose' : 'Do you have a Runny-Nose? [Yes, No]',
+    'Nasal-Congestion' : 'Do you have Nasal-Congestion? [Yes, No]',
+    'Pains' : 'Do you have Pains? [Yes, No]',
+    'None_Sympton' : 'Do you have any symptoms? [Yes, No]',
+    'Sore-Throat' : 'Do you have a Sore-Throat? [Yes, No]',
+    'Difficulty-in-Breathing': 'Do you have Difficulty-in-Breathing? [Yes, No]',
+    'Dry-Cough' : 'Do you have a Dry-Cough? [Yes, No]',
+    'Tiredness' : 'Do you have Tiredness? [Yes, No]',
+    'Fever' : 'Do you have a Fever? [Yes, No]',
+} # type: ignore
 curr_question = 0
 
 
@@ -11,6 +27,7 @@ curr_question = 0
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/get-response", methods=["POST"])
 def get_response():
@@ -25,18 +42,14 @@ def get_response():
 
 @app.route("/get-question", methods=["POST"])
 def get_question():
-    question = questions.items()[curr_question]
+    question = questions.items()
     return jsonify({"question": question, "column": column})
 
 
-def ask_question(question, column):
-    return jsonify({"next_question": question, "column": column})
+
 
 
 def handle_response(response, accepted_responses, column):
     while response not in accepted_responses:
         response = input("Sorry, I didn't understand that. Please try again: ")
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
