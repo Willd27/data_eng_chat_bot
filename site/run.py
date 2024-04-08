887eb6c027112411ac4c9db414db2dcc65730ea4
gabriiellllll <60512228+gabriiellllll@users.noreply.github.com>
2024-04-08 09:23:52 +0200

    feat: site good

--- site/run.py
+++ site/run.py
@@ -1,4 +1,4 @@
-from chat_bot import app
+from src.chat_bot import app
 
 if __name__ == "__main__":
     app.run(port=5555)
--- site/static/script.js
+++ site/static/script.js
@@ -1,3 +1,72 @@
+var map = L.map('map').setView([0, 0], 2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+}).addTo(map);
+
+var markers = {};
+
+function updateMap(country, count) {
+    // Utilisez l'API de géolocalisation Nominatim pour obtenir les coordonnées du pays
+    fetch(`https://nominatim.openstreetmap.org/search?country=${country}&format=json`)
+    .then(response => response.json())
+    .then(data => {
+        if (data.length > 0) {
+            const lat = parseFloat(data[0].lat);
+            const lon = parseFloat(data[0].lon);
+            if (!markers[country]) {
+                markers[country] = L.circleMarker([lat, lon], {
+                    radius: 5,
+                    fillColor: "#ff7800",
+                    color: "#000",
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.8
+                }).addTo(map);
+            }
+            markers[country].setRadius(count);
+        } else {
+            console.error(`Coordinates not found for ${country}`);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+
+function updateMapFromServer() {
+    fetch('/update_map', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        var countryCounts = {};
+        data.countries.forEach(country => {
+            if (countryCounts[country]) {
+                countryCounts[country]++;
+            } else {
+                countryCounts[country] = 1;
+            }
+        });
+
+        // Parcourez le tableau des pays et de leurs comptages
+        for (const [country, count] of Object.entries(countryCounts)) {
+            updateMap(country, count); // Mettre à jour la carte pour chaque pays avec son comptage
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // Gérez les erreurs ici
+    });
+}
+
+// Call updateMapFromServer when the page loads
+window.addEventListener('load', updateMapFromServer);
+
 function sendMessage(message) {
     fetch('/get_question', {
         method: 'POST',
@@ -10,8 +79,11 @@ function sendMessage(message) {
     .then(data => {
         if (data.end) {
             displayMessage(data.end);
+            
         } else {
             displayQuestion(data.question);
+            updateMapFromServer();
+            // Assuming you have the selected country in data
         }
     })
     .catch(error => {
@@ -41,7 +113,6 @@ function sendMessageHandler() {
 
 document.getElementById('send-button').addEventListener('click', sendMessageHandler);
 
-
 function resetChatSession() {
     const chatBox = document.getElementById('chat-box');
     chatBox.innerHTML = 'Welcome to COVID BOT'; // Clear chat history
@@ -61,7 +132,6 @@ function resetChatSession() {
 
 // Add event listener to the "Reset" button
 document.getElementById('reset-button').addEventListener('click', resetChatSession);
-
 
 // Add this function to initiate the conversation when the page loads
 function initiateConversation() {
@@ -87,3 +157,5 @@ function initiateConversation() {
 
 // Call initiateConversation when the page loads
 window.addEventListener('load', resetChatSession);
+
+
